2dd5e01b9bc6c7702f8641784e64fe8400b5b88a
kahmyl <kamilhabib556@gmail.com>
2022-09-19 06:30:08 +0100

    added authentication middleware

---
+++ src/common/utils/authentication.ts
@@ -0,0 +1,66 @@
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import config, { AccessTokenMaxAge, RefreshTokenMaxAge } from 'src/config';
+
+type TokenKeys = {
+  user_refresh_key: string;
+  user_access_key: string;
+};
+
+type TokenKey = keyof typeof tokenKeys;
+
+const tokenKeys: TokenKeys = {
+  user_access_key: config.config().keys.ACCESS_KEY,
+  user_refresh_key: config.config().keys.REFRESH_KEY,
+};
+
+export const hashPassword = async (password: string) => {
+  return await bcrypt.hash(password, 10);
+};
+
+export const comparePassword = async (password: string, hash: string) => {
+  return await bcrypt.compare(password, hash);
+};
+
+export const generateAccessToken = (
+  id: string,
+  domain: TokenKey = 'user_access_key',
+) => {
+  return jwt.sign({ id }, tokenKeys[domain], {
+    expiresIn: AccessTokenMaxAge / 1000,
+  });
+};
+
+export const generateRefreshToken = (
+  id: string,
+  domain: TokenKey = 'user_refresh_key',
+) => {
+  return jwt.sign({ id }, tokenKeys[domain], {
+    expiresIn: RefreshTokenMaxAge / 1000,
+  });
+};
+
+export const verifyJWT = (token: string, key: string): any | boolean => {
+  try {
+    const resp: any = jwt.verify(token, key);
+    return resp;
+  } catch {
+    return false;
+  }
+};
+
+export const verifyAccessToken = (token: string) => {
+  const resp = verifyJWT(token, config.config().keys.ACCESS_KEY);
+  if (resp) {
+    return resp;
+  }
+  return false;
+};
+
+export const verifyRefreshToken = (token: string) => {
+  const resp = verifyJWT(token, config.config().keys.REFRESH_KEY);
+  if (resp) {
+    return resp;
+  }
+  return false;
+};
