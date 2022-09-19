636ca8ff8afe85edefc0b8c73e9cc9130bd99238
kahmyl <kamilhabib556@gmail.com>
2022-09-19 06:32:07 +0100

    Updated app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbconfig } from './config/db';
+import { UserModule } from './user/user.module';
+import { BrandModule } from './brand/brand.module';
 
 @Module({
   imports: [
@@ -12,7 +14,8 @@ import { dbconfig } from './config/db';
       load: [],
     }),
     TypeOrmModule.forRoot(dbconfig.getTypeOrmConfig()),
-    
+    UserModule,
+    BrandModule
   ],
   controllers: [AppController],
   providers: [AppService],
