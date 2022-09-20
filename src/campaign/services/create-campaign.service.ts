b69354e76e33e17f82b56a2e59837e654d3e0840
kahmyl <kamilhabib556@gmail.com>
2022-09-20 08:31:52 +0100

    Checking if actual balance is not empty before creating campaign

--- src/campaign/services/create-campaign.service.ts
+++ src/campaign/services/create-campaign.service.ts
@@ -1,5 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UnprocessableEntityErrorException } from 'src/common/filters/error-exceptions';
+import { MockTransactionService } from 'src/wallet/services/mock-transaction.service';
+import { Price, TransactionType } from 'src/types';
+import { Wallet } from 'src/wallet/entity/wallet.entity';
+import { WalletBalanceService } from 'src/wallet/services/wallet-balance.service';
 import { Repository } from 'typeorm';
 import { CreateCampaignDto } from '../dto/campaign.dto';
 import { Campaign } from '../entity/campaign.entity';
@@ -10,11 +15,29 @@ export class CreateCampaignService {
   constructor(
     @InjectRepository(Campaign)
     private readonly campaignRepo: Repository<Campaign>,
+    @InjectRepository(Wallet)
+    private readonly walletRepo: Repository<Wallet>,
+    private readonly walletBalance: WalletBalanceService,
+    private readonly mockTransaction: MockTransactionService,
   ) {}
 
   async execute(data: CreateCampaignDto) {
     try {
       const payload = new CreateCampaignDto().toEntity(data);
+      const { brand_id } = payload;
+      const wallet = await this.walletRepo.findOne({ where: { brand_id } });
+      const { actual_balance } = await this.walletBalance.execute(wallet.id);
+      if (actual_balance.amount < Price) {
+        throw new UnprocessableEntityErrorException(
+          'Insufficient Funds in wallet',
+        );
+      }
+      await this.mockTransaction.execute(
+        Price,
+        TransactionType.DESK,
+        brand_id,
+        `funding ${payload.name}`,
+      );
       const result = await this.campaignRepo.save(payload);
 
       return await this.prepareCampaignResponseData(result);
